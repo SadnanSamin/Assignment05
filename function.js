86f84270f94f93419816e28d5d5166a40a58fe5d
SadnanSamin <sadnanislamsamin001@gmail.com>
2025-08-29 03:27:21 +0600

    worked on the functionality of the history section , call button and many more

--- function.js
+++ function.js
@@ -5,14 +5,14 @@ for (const heart of hearts) {
     heart.addEventListener("click", function () {
         const currentCount = parseInt(document.getElementById("heart-number").innerText);
 
-        if(click === false){
-            const newCount = currentCount+1;
+        if (click === false) {
+            const newCount = currentCount + 1;
             click = true;
             document.getElementById("heart-number").innerText = newCount;
             heart.style.color = "red";
         }
-        else{
-            const newCount = currentCount -1;
+        else {
+            const newCount = currentCount - 1;
             click = false
             document.getElementById("heart-number").innerText = newCount;
             heart.style.color = "";
@@ -32,8 +32,8 @@ for (const copyButton of copyButtons) {
         navigator.clipboard.writeText(copyNumber)
             .then(() => {
                 alert(`${copyNumber} is copied to the clipboard`);
-                const newCount = copyCount+1;
-                document.getElementById("copyCount").innerText=newCount;
+                const newCount = copyCount + 1;
+                document.getElementById("copyCount").innerText = newCount;
             })
             .catch(err => {
                 console.error("Failed to copy: ", err);
@@ -41,3 +41,67 @@ for (const copyButton of copyButtons) {
 
     })
 }
+
+// call functionality
+
+const history = [];
+
+const callButtons = document.getElementsByClassName("callButton");
+
+for (const callButton of callButtons) {
+
+    callButton.addEventListener("click", function () {
+
+        const coinCount = parseInt(document.getElementById("coinCount").innerText);
+        const callerID = callButton.parentElement.parentElement;
+        const heading = callerID.querySelector(".heading").innerText;
+        const callerNumber = callerID.querySelector(".card-number").innerText;
+
+        if (coinCount >= 20) {
+            alert(`Calling ${heading} ${callerNumber} ...`);
+            const newCoinCount = coinCount - 20;
+            document.getElementById("coinCount").innerText = newCoinCount;
+
+            const storeHistory = document.getElementById("callerHistory");
+
+
+            const callHistory = {
+                name: heading,
+                number: callerNumber,
+                time: new Date().toLocaleTimeString()
+            };
+
+
+            history.push(callHistory);
+
+
+            const div = document.createElement("div");
+            div.innerHTML = `
+                <div class ="bg-[#FAFAFA] hind-madurai-font flex justify-between items-center my-4 p-4 rounded-[8px]">
+                    <div>
+                    <h1 class="inter-font font-semibold text-[18px]">${callHistory.name}</h1>
+                    <h1 class="text-[18px] text-gray-500 ">${callHistory.number}</h1>
+                </div>
+                <div>
+                    <h1 class="text-[18px] ">${callHistory.time}</h1>
+                </div>
+                </div>
+            `;
+            storeHistory.appendChild(div);
+
+        } else {
+            alert("Not enough coin to make a call");
+        }
+    });
+}
+
+
+// clear button functionality
+
+document.getElementById("clearButton").addEventListener("click", function () {
+    const storeHistory = document.getElementById("callerHistory");
+    storeHistory.innerText = " ";
+})
+
+
+
